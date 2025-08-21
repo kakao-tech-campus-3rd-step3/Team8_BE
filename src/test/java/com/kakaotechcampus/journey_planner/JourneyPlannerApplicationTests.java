575212da962d16e2fac584f470c96626243915b1
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-08-21 13:27:00 +0900

    Feat: First Setting Of SpringBoot

---
+++ src/test/java/com/kakaotechcampus/journey_planner/JourneyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotechcampus.journey_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
