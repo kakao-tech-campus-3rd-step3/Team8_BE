7989d0f4e68ad9bc8b3af9b6636f05c7fb86c5e4
thdgmlwns1 <xhrl233@naver.com>
2025-08-29 00:28:01 +0900

    feat(plan): plan domain 생성

---
+++ src/main/java/com/kakaotechcampus/journey_planner/domain/plan/Plan.java
@@ -0,0 +1,32 @@
+package com.kakaotechcampus.journey_planner.domain.plan;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    public Plan(String title, String description, LocalDate startDate, LocalDate endDate) {
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
