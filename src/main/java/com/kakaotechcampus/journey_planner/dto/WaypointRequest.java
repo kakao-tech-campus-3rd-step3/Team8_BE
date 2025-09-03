7f1e7aba20e0d8fe367686871a9841250b4322d7
YoonGangHoon <yghun021007@naver.com>
2025-09-03 17:31:31 +0900

    Feat: 응답 형식에 따른 DTO 생성

---
+++ src/main/java/com/kakaotechcampus/journey_planner/dto/WaypointRequest.java
@@ -0,0 +1,17 @@
+package com.kakaotechcampus.journey_planner.dto;
+
+import java.time.LocalDateTime;
+
+public record WaypointRequest(
+        String name,
+        LocationDto location,
+        LocalDateTime arriveTime,
+        String locationType,
+        String memo
+) {
+    public record LocationDto(
+            String address,
+            Double latitude,
+            Double longitude
+    ) {}
+}
---
+++ src/main/java/com/kakaotechcampus/journey_planner/dto/WaypointResponse.java
@@ -0,0 +1,18 @@
+package com.kakaotechcampus.journey_planner.dto;
+
+import java.time.LocalDateTime;
+
+public record WaypointResponse(
+        Long id,
+        String name,
+        LocationDto location,
+        LocalDateTime arriveTime,
+        String locationType,
+        String memo
+) {
+    public record LocationDto(
+            String address,
+            Double latitude,
+            Double longitude
+    ) {}
+}
