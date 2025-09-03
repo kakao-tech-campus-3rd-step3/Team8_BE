ceb81fbcbd4f033f857f907327e233fe425f8a76
YoonGangHoon <yghun021007@naver.com>
2025-09-03 17:34:04 +0900

    Feat: 웨이포인트 생성, 삭제 기능 추가

---
+++ src/main/java/com/kakaotechcampus/journey_planner/domain/waypoint/Waypoint.java
@@ -0,0 +1,45 @@
+package com.kakaotechcampus.journey_planner.domain.waypoint;
+
+import com.kakaotechcampus.journey_planner.domain.plan.Plan;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Waypoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+    private String name;
+
+    @Embedded
+    private Location location;
+
+    private LocalDateTime arriveTime;
+
+    private String locationType;
+
+    private String memo;
+
+    protected Waypoint() {}
+
+    public Waypoint(String name, Location location, LocalDateTime arriveTime, String locationType, String memo) {
+        this.name = name;
+        this.location = location;
+        this.arriveTime = arriveTime;
+        this.locationType = locationType;
+        this.memo = memo;
+    }
+
+    public void assignToPlan(Plan plan) {
+        this.plan = plan;
+    }
+}
