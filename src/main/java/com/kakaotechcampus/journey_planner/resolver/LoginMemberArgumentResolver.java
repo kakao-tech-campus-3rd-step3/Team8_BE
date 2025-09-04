e05179c66ccefcb861fb9305b158cb6cccce9649
milk-stone <ywshero@naver.com>
2025-09-05 03:43:56 +0900

    feat(auth): accessToken이 유효할 때 인증된 Member 객체를 바로 받아올 수 있게 하는 resolver 클래스, @LoginMember 작성

---
+++ src/main/java/com/kakaotechcampus/journey_planner/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,51 @@
+package com.kakaotechcampus.journey_planner.resolver;
+
+import com.kakaotechcampus.journey_planner.annotation.LoginMember;
+import com.kakaotechcampus.journey_planner.application.auth.jwt.JwtProvider;
+import com.kakaotechcampus.journey_planner.application.member.MemberService;
+import com.kakaotechcampus.journey_planner.global.exception.CustomRuntimeException;
+import com.kakaotechcampus.journey_planner.global.exception.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
+
+    public LoginMemberArgumentResolver(JwtProvider jwtProvider, MemberService memberService) {
+        this.jwtProvider = jwtProvider;
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = extractToken(authorizationHeader);
+        String email = jwtProvider.extractEmailFromAccessToken(token);
+        return memberService.findByEmail(email)
+                .orElseThrow(() -> new CustomRuntimeException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private String extractToken(String authorizationHeader) {
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        throw new IllegalArgumentException("유효하지 않은 인증 헤더입니다.");
+    }
+}
+
