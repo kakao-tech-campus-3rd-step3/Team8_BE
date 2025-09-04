3c63ea71114242613ce0404359da1f522689ba21
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-04 10:29:12 +0900

    Feat: PDF Util File moved To PDF Infra layer (#4)

---
+++ src/main/java/com/kakaotechcampus/journey_planner/infra/iTextPdf/PdfGenerator.java
@@ -0,0 +1,34 @@
+package com.kakaotechcampus.journey_planner.infra.iTextPdf;
+
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+
+@Component
+public class PdfGenerator {
+    private final static String htmlFileSource = "/templates/html/PdfTemplate.html";
+
+    /*
+     * Make PDF with ITextPdf library
+     * filename - PDF Title what you want to use
+     * **/
+    public void createPdf(ByteArrayOutputStream baos) throws FileNotFoundException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, baos);
+        document.open();
+
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("테스트용으로 작성중입니다.", font);
+
+        document.add(chunk);
+        document.close();
+    }
+
+    public void createPlanPdf(ByteArrayOutputStream baos) throws IOException {
+        File htmlSource = new File(htmlFileSource);
+        HtmlConverter.convertToPdf(new FileInputStream(htmlSource), baos);
+    }
+}
