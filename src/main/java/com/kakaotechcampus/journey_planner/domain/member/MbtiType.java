83421a161acf3ec53d6412f2858e1dd6063535fa
milk-stone <ywshero@naver.com>
2025-09-05 03:40:48 +0900

    feat(member): Member 엔티티 정립, 서비스, 레포지토리 기본 코드 작성, findByEmail 메서드 작성

---
+++ src/main/java/com/kakaotechcampus/journey_planner/domain/member/MbtiType.java
@@ -0,0 +1,5 @@
+package com.kakaotechcampus.journey_planner.domain.member;
+
+public enum MbtiType {
+    INFJ, INFP, INTJ, INTP, ISFJ, ISFP, ISTJ, ISTP, ENFJ, ENFP, ENTJ, ENTP, ESFJ, ESFP, ESTJ, ESTP
+}
