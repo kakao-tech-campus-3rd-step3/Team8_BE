0b9eb31a13b7e78acde9847d64dd60570cfc3eaa
YoonGangHoon <yghun021007@naver.com>
2025-09-06 17:40:02 +0900

    refactor: 사용되지 않는 라이브러리 제거

--- src/main/java/com/kakaotechcampus/journey_planner/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakaotechcampus/journey_planner/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.kakaotechcampus.journey_planner.global.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
