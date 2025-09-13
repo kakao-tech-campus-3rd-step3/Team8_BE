ce5a4696b9cfceb7bcbd6cce5c21b546e17c1821
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-14 06:35:36 +0900

    Feat: Service Layer Simplify (#19)

--- src/main/java/com/kakaotechcampus/journey_planner/application/pdf/PdfService.java
+++ src/main/java/com/kakaotechcampus/journey_planner/application/pdf/PdfService.java
@@ -1,34 +1,17 @@
 package com.kakaotechcampus.journey_planner.application.pdf;
 
-import com.itextpdf.text.DocumentException;
-import com.kakaotechcampus.journey_planner.global.exception.CustomRuntimeException;
-import com.kakaotechcampus.journey_planner.global.exception.ErrorCode;
-import com.kakaotechcampus.journey_planner.infra.iTextPdf.PdfGenerator;
+import com.kakaotechcampus.journey_planner.domain.pdf.PdfGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.OutputStream;
 
 @Service
 @RequiredArgsConstructor
 public class PdfService {
-    private final PdfGenerator pdfGenerator;
+    public final PdfGenerator pdfGenerator;
 
-    public void createPdfFile(ByteArrayOutputStream baos){
-        try{
-            pdfGenerator.createPdf(baos);
-        }catch (FileNotFoundException | DocumentException e){
-            throw new CustomRuntimeException(ErrorCode.NO_FILE);
-        }
-    }
-
-    public void createHtmlPdfFile(ByteArrayOutputStream baos){
-        try{
-            pdfGenerator.createPlanPdf(baos);
-        }catch(IOException e){
-            throw new RuntimeException(e.getMessage());
-        }
+    public void createPDF(OutputStream outputStream) {
+        pdfGenerator.createPdf(outputStream);
     }
 }
