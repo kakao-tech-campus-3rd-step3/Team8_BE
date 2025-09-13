8798800142ff9c8160148405cdfdd0d4ebad6bd0
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-14 06:08:26 +0900

    Feat: ErrorCode added (#19)

--- src/main/java/com/kakaotechcampus/journey_planner/global/exception/ErrorCode.java
+++ src/main/java/com/kakaotechcampus/journey_planner/global/exception/ErrorCode.java
@@ -1,16 +1,28 @@
 package com.kakaotechcampus.journey_planner.global.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    NO_FILE(HttpStatus.NOT_FOUND, "NO_FILE","파일 경로를 찾을 수 없습니다."),
-    PLAN_NOT_FOUND(HttpStatus.NOT_FOUND, "PLAN_NOT_FOUND", "해당 계획을 찾을 수 없습니다.");
+    // * GLOBAL
 
-    private HttpStatus status;
+    // * PLAN
+    NO_FILE(HttpStatus.NOT_FOUND, "NO_FILE","파일 경로를 찾을 수 없습니다."),
+    PLAN_NOT_FOUND(HttpStatus.NOT_FOUND, "PLAN_NOT_FOUND", "해당 계획을 찾을 수 없습니다."),
+
+    // * Resource
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "NO_RESOURCE", "리소스를 찾을 수 없습니다."),
+    FONT_NOT_FOUND(HttpStatus.NOT_FOUND, "NO_FONT_RESOURCE", "폰트를 찾을 수 없습니다."),
+
+
+    // * PDF
+    CANNOT_CREATE(HttpStatus.INTERNAL_SERVER_ERROR, "CANNOT_CREATE_PDF", "PDF 를 생성할 수 없습니다.")
+    ;
+    
+    private final HttpStatus status;
     private final String code;
-    private String message;
+    private final String message;
 }
