7a740f8e817651baba57c8661d7c317b1d89be28
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-14 06:13:01 +0900

    Feat: Exception naming changed (#19)

--- src/main/java/com/kakaotechcampus/journey_planner/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakaotechcampus/journey_planner/global/exception/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.kakaotechcampus.journey_planner.global.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(CustomRuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomRuntimeException e) {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(BusinessException e) {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(new ErrorResponse(e.getCode(), e.getMessage()));
