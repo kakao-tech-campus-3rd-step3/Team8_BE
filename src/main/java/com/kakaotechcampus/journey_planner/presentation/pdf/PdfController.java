9c25c8eee61b842d91afd05223ccdbb5d18ad3cb
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-14 07:03:54 +0900

    Feat: Pdf Download Method changed to Streaming method (#19)

---
+++ src/main/java/com/kakaotechcampus/journey_planner/presentation/pdf/PdfController.java
@@ -0,0 +1,23 @@
+package com.kakaotechcampus.journey_planner.presentation.pdf;
+
+import com.kakaotechcampus.journey_planner.presentation.support.PdfResponseFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+@RestController
+@RequestMapping("/v1/files")
+@RequiredArgsConstructor
+public class PdfController {
+    private final PdfResponseFactory pdfResponseFactory;
+
+    @GetMapping
+    public ResponseEntity<StreamingResponseBody> downloadJourneyPlan(){
+        return ResponseEntity.ok()
+                .headers(pdfResponseFactory.getPdfHeader(""))
+                .body(pdfResponseFactory.linkToPdfFile());
+    }
+}
