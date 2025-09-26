b7e4ddad170f7a86f744e3d6119b59617641e358
milk-stone <ywshero@naver.com>
2025-09-26 21:51:37 +0900

    feat(memberPlan): MemberPlan Service, Repository 코드 작성
    member와 plan을 이용해 memberPlan 객체를 생성하고 저장하는 create 메서드 작성

---
+++ src/main/java/com/kakaotechcampus/journey_planner/domain/memberplan/MemberPlanRepository.java
@@ -0,0 +1,6 @@
+package com.kakaotechcampus.journey_planner.domain.memberplan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPlanRepository extends JpaRepository<MemberPlan, Long> {
+}
