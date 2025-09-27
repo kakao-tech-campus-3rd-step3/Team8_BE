390f2f80a35979411bb96adf26b34ab4704e1cf1
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-09-27 21:33:06 +0900

    Feat: 동일 기능하는 Config 한 파일로 응축

--- src/main/java/com/kakaotechcampus/journey_planner/global/config/WebMvcConfig.java
+++ src/main/java/com/kakaotechcampus/journey_planner/global/config/WebMvcConfig.java
@@ -1,11 +1,24 @@
 package com.kakaotechcampus.journey_planner.global.config;
 
+import com.kakaotechcampus.journey_planner.global.resolver.LoginMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
