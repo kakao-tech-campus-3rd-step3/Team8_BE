41f910a4b784c01494462abe47d7de192421a3b6
YoonGangHoon <yghun021007@naver.com>
2025-09-30 22:27:53 +0900

    fix: 응답 무한 제귀 임시 조치

--- src/main/java/com/kakaotechcampus/journey_planner/presentation/plan/dto/response/PlanResponse.java
+++ src/main/java/com/kakaotechcampus/journey_planner/presentation/plan/dto/response/PlanResponse.java
@@ -1,18 +1,14 @@
 package com.kakaotechcampus.journey_planner.presentation.plan.dto.response;
 
-import com.kakaotechcampus.journey_planner.domain.member.Member;
 import com.kakaotechcampus.journey_planner.domain.plan.Plan;
-import com.kakaotechcampus.journey_planner.domain.traveler.Traveler;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public record PlanResponse(
         Long id,
         String title,
         String description,
-        Member organizer,
-        List<Traveler> travelers,
+        // List<Traveler> travelers,
         LocalDate startDate,
         LocalDate endDate
 ) {
@@ -21,8 +17,7 @@ public record PlanResponse(
                 plan.getId(),
                 plan.getTitle(),
                 plan.getDescription(),
-                plan.getOrganizer(),
-                plan.getTravelers(),
+                // plan.getTravelers(),
                 plan.getStartDate(),
                 plan.getEndDate()
         );
