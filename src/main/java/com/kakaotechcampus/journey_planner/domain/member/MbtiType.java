6fa1cdb272ea465ea99488147d0b2d3fa1d5d5b3
milk-stone <ywshero@naver.com>
2025-11-04 22:41:02 +0900

    fix(member): 사용자 정보 수정 시 mbti가 소문자로 들어올 경우에도 정상적으로 만들어지도록 수정

--- src/main/java/com/kakaotechcampus/journey_planner/domain/member/MbtiType.java
+++ src/main/java/com/kakaotechcampus/journey_planner/domain/member/MbtiType.java
@@ -1,5 +1,15 @@
 package com.kakaotechcampus.journey_planner.domain.member;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum MbtiType {
-    INFJ, INFP, INTJ, INTP, ISFJ, ISFP, ISTJ, ISTP, ENFJ, ENFP, ENTJ, ENTP, ESFJ, ESFP, ESTJ, ESTP
+    INFJ, INFP, INTJ, INTP, ISFJ, ISFP, ISTJ, ISTP, ENFJ, ENFP, ENTJ, ENTP, ESFJ, ESFP, ESTJ, ESTP;
+
+    @JsonCreator
+    public static MbtiType from(String s) {
+        if (s == null) {
+            return null;
+        }
+        return MbtiType.valueOf(s.toUpperCase());
+    }
 }
